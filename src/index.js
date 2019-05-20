b9b3967109ac8b3a090cf7a0172aa2d022437a4d
xhzGitHub <739809951@qq.com>
2019-05-20 17:36:18 +0800

    add react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd-mobile' 
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Register from './containers/register/register'
+import Login from './containers/login/login'
+import Main from './containers/main/main'
+
 
 ReactDOM.render(
-  <Button type="primary">Test</Button>,
+  <HashRouter>
+    <Switch>
+      <Route path="/register" component={Register}></Route>
+      <Route path="/login" component={Login}></Route>
+      <Route component={Main}></Route>
+    </Switch>
+  </HashRouter>,
   document.getElementById('root')
 );
